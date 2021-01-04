ded30f7bfc57b6a5605df649368a315fcce63708
gui-kev <56992693+gui-kev@users.noreply.github.com>
2021-01-04 15:20:55 +0100

    Kevin GUIFFO KUICHEU : Test class City

--- src/test/java/tsp/lee/jacobson/CityTest.java
+++ src/test/java/tsp/lee/jacobson/CityTest.java
@@ -7,8 +7,35 @@ import org.junit.Test;
 public class CityTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	/**
+	 * Given distanceTo
+	 * When two cities are same 
+	 * Then distance = 0
+	 */
+	public void distanceTo_Same_0() {
+		double expected = 0;
+		City c1 = new City(-20,20);
+		City c2 = new City(-20,20);
+		
+		double actual = c1.distanceTo(c2);	
+		
+		assertEquals(actual, expected,0);	
 	}
-
+	
+	@Test
+	/**
+	 * Given distanceTo
+	 * When two cities are different
+	 * Then distance = sqrt[(x.city1-x.city2)² + (y.city1-y.city2)²]
+	 */
+	public void distanceTo_HorizentalyAlligned_DiffAbscices() {
+		City c1 = new City(-5,1);
+		City c2 = new City(18,15);
+		double expected = Math.sqrt((-5-18)*(-5-18) + (1-15)*(1-15));
+		
+		double actual = c1.distanceTo(c2);
+		
+		assertEquals(actual, expected,0);	
+	}
+	
 }
